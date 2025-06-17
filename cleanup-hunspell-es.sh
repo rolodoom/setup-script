b167f34f7be9836ef2bf2bbdb7505a38432cc24f
RoLo <rolodoom@gmail.com>
2025-06-16 20:15:25 -0500

    :bug: Fix typo and add lib notify.sh

--- cleanup-hunspell-es.sh
+++ cleanup-hunspell-es.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-echo "-> CXleanup hunspell-ES..."
-# Remove unwanted Spanish dictionaries, keep only es_ES and es_CO
 
+# --- External Functions ---
+
+# Load the notify function
+source lib/notify_lib.sh
+
+notify "-> Cleanup hunspell-ES..." "heading"
+
+# Remove unwanted Spanish dictionaries, keep only es_ES and es_CO
 for lang in es_ANY es_AR es_BO es_CL es_CR es_CU es_DO es_EC es_GT es_HN es_MX es_NI es_PA es_PR es_PY es_PE es_SV es_UY es_VE; do
     sudo rm -f /usr/share/hunspell/${lang}.*
 done
