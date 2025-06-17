c2f77ac29a4ffb0f4c492674bb5d03ac7c55b148
RoLo <rolodoom@gmail.com>
2025-06-16 20:09:55 -0500

    Add Flatpak apps from flathub

--- install-flatpak.sh
+++ install-flatpak.sh
@@ -5,7 +5,5 @@
 # Load the notify function
 source lib/notify_lib.sh
 
-notify "Installing flatpak & flathub for Debian KDE" "heading"
-sudo apt install flatpak -y
-sudo apt install plasma-discover-backend-flatpak -y
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+notify "Installing Flatpak software from Flathub.org" "heading"
+sudo flatpak install com.calibre_ebook.calibre com.github.tchx84.Flatseal com.polyphone_soundfonts.polyphone com.sigil_ebook.Sigil com.spotify.Client io.github.giantpinkrobots.flatsweep io.gitlab.news_flash.NewsFlash org.audiveris.audiveris org.gnome.gitlab.dqpb.GMetronome org.kde.kcolorchooser org.libretro.RetroArch org.mozilla.Thunderbird org.nickvision.tubeconverter org.torproject.torbrowser-launcher
