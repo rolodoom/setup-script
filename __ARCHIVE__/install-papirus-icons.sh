e6bebef40f27efb932ba8219079715d11c245f81
RoLo <rolodoom@gmail.com>
2025-06-19 05:58:46 -0500

    remove unused files

--- __ARCHIVE__/install-papirus-icons.sh
+++
@@ -1,5 +0,0 @@
-echo "-> Installing papirus icons..."
-sudo sh -c "echo 'deb http://ppa.launchpad.net/papirus/papirus/ubuntu jammy main' > /etc/apt/sources.list.d/papirus-ppa.list"
-sudo wget -qO /etc/apt/trusted.gpg.d/papirus-ppa.asc 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x9461999446FAF0DF770BFC9AE58A9D36647CAE7F'
-sudo apt-get update
-sudo apt-get install papirus-icon-theme -y
--- __ARCHIVE__/install-ulauncher-materia-dark-theme.sh
+++
@@ -1,4 +0,0 @@
-echo "-> Installing ulauncher dark theme..."
-mkdir -p ~/.config/ulauncher/user-themes
-git clone https://github.com/levonhart/materia-dark-ulauncher \
-  ~/.config/ulauncher/user-themes/materia-dark-ulauncher
