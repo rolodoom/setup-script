7613f90ae5ec8eb1c72884e19d90131c890667d0
RoLo <rolodoom@gmail.com>
2025-06-22 16:05:05 -0500

    Clean flatpaks and add repo apps

--- install-flatpak-apps.sh
+++ install-flatpak-apps.sh
@@ -6,4 +6,4 @@
 source lib/notify_lib.sh
 
 notify "Installing Flatpak software from Flathub.org" "heading"
-sudo flatpak install com.calibre_ebook.calibre com.github.tchx84.Flatseal com.polyphone_soundfonts.polyphone com.sigil_ebook.Sigil com.spotify.Client io.github.giantpinkrobots.flatsweep io.gitlab.news_flash.NewsFlash org.audiveris.audiveris org.gnome.gitlab.dqpb.GMetronome org.kde.kcolorchooser org.libretro.RetroArch org.mozilla.Thunderbird org.nickvision.tubeconverter org.torproject.torbrowser-launcher
+sudo flatpak install com.github.tchx84.Flatseal com.polyphone_soundfonts.polyphone com.sigil_ebook.Sigil com.spotify.Client io.github.giantpinkrobots.flatsweep io.gitlab.news_flash.NewsFlash org.audiveris.audiveris org.gnome.gitlab.dqpb.GMetronome org.kde.kcolorchooser org.libretro.RetroArch org.nickvision.tubeconverter org.torproject.torbrowser-launcher
