414bc3b987a1364e3941957310f04cb35e911ef4
RoLo <rolodoom@gmail.com>
2025-06-26 15:30:13 -0500

    :bug: Fix typing error

--- install-brave.sh
+++ install-brave.sh
@@ -5,6 +5,6 @@
 # Load the notify function
 source lib/notify_lib.sh
 
-notify "Installing asciiquarium" "heading"
+notify "Installing brave" "heading"
 
 curl -fsS https://dl.brave.com/install.sh | sh
