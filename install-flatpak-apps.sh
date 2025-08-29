73067e0b85291e2baf3fbb411536f4bd16282c6f
RoLo <rolodoom@gmail.com>
2025-08-29 06:08:39 -0500

    Add Carla

--- install-flatpak-apps.sh
+++ install-flatpak-apps.sh
@@ -6,4 +6,4 @@
 source lib/notify_lib.sh
 
 notify "Installing Flatpak software from Flathub.org" "heading"
-sudo flatpak install com.github.tchx84.Flatseal com.sigil_ebook.Sigil com.spotify.Client io.github.giantpinkrobots.flatsweep org.audiveris.audiveris org.gnome.gitlab.dqpb.GMetronome org.libretro.RetroArch org.nickvision.tubeconverter org.torproject.torbrowser-launcher
+sudo flatpak install flathub studio.kx.carla com.github.tchx84.Flatseal com.sigil_ebook.Sigil com.spotify.Client io.github.giantpinkrobots.flatsweep org.audiveris.audiveris org.gnome.gitlab.dqpb.GMetronome org.libretro.RetroArch org.nickvision.tubeconverter org.torproject.torbrowser-launcher
