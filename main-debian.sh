6527422efc70a9408ea874e3bab08920ae77bd18
RoLo <rolodoom@gmail.com>
2025-09-28 10:02:25 -0500

    Enable PREEMPT_FULL at boot via GRUB

--- main-debian.sh
+++ main-debian.sh
@@ -30,7 +30,7 @@ sudo apt install -y wget curl htop btop git rsync zsh
 
 notify "Configure System for Audio Workstation"
 notify "grub ..."
-sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/g' /etc/default/grub
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash preempt=full"/g' /etc/default/grub
 sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 sudo update-grub
 
